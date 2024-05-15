e94afcafb78e7beef9e9abeb6ff6ae611fbcda3d
A-Bekirovik <A.Bekirovik@hotmail.com>
2024-05-15 10:28:55 +0200

    Added UserService

---
+++ MythWikiLogic/Service/UserService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MythWikiDAL.DTO;
+using MythWikiDAL.Repository;
+
+namespace MythWikiLogic.Services
+{
+    public class UserService 
+    {
+        List<UserDTO> users = new List<UserDTO>();
+
+        private readonly UserRepository _userRepository;
+
+        public UserService()
+        {
+            _userRepository = new UserRepository();
+        }
+
+        public List<UserDTO> GetAllUsers()
+        {
+            users = _userRepository.GetAllUsers();
+            return users;
+        }
+    }
+}
